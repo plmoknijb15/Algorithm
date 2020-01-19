d4b1b1509b8207471dd9fdf95160327e28f8f8cf
username1015 <plmoknijb15@naver.com>
2020-01-19 22:50:29 +0900

    N과 M(2)

---
+++ Brute_force/boj_15650.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+void func(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 1; i < v.size(); i++)
+            cout << v[i] << " ";
+        cout << endl;
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (v.back() < i)
+        {
+            v.push_back(i);
+            func(cnt + 1);
+            v.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    v.push_back(0);
+
+    func(0);
+        
+    return 0;
+}
