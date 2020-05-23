227f8d07f58349fab9f483cd1befd6ee470089f6
username1015 <plmoknijb15@naver.com>
2020-05-24 02:56:46 +0900

    로봇청소기

--- Etc/boj_14503.cpp
+++ Etc/boj_14503.cpp
@@ -2,20 +2,55 @@
 
 using namespace std;
 
-int map[51][51];
+int map[50][50];
+int count;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
 
-int main()
-{
+int main() {
     int n, m, r, c, d;
     cin >> n >> m >> r >> c >> d;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; i <= m; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> map[i][j];
-            cout << map[i][j] << " "; 
+
+    while (true) {
+        if (map[r][c] == 0) {
+            count++;
+            map[r][c] = 2;
+        }   
+
+        bool go_back = false;
+        for (int i = 0; i <= 3; i++) {
+            int check_x = r + dx[(d+3-i) % 4];
+            int check_y = c + dy[(d+3-i) % 4];
+            
+            if (map[check_x][check_y] == 0) {
+                r = check_x;
+                c = check_y;
+                d = (d+3-i) % 4;
+                break;
+            }
+
+            if (i == 3)
+                go_back = true;
         }
-        cout << endl;
-    }
         
+        if (go_back) {
+            int check_x = r + dx[(d+2) % 4];
+            int check_y = c + dy[(d+2) % 4];
+
+            if (map[check_x][check_y] != 1) {
+                r = check_x;
+                c = check_y;
+            }
+            else
+                break;
+        }
+    }
+    
+    cout << count << endl;
+
     return 0;
 }
